ff562dc3ed53e9b9c5c32f5963530da767e27859
AnthonyAposta <39099418+AnthonyAposta@users.noreply.github.com>
2019-02-08 13:48:42 -0200

    Update perceptron.py

--- perceptron.py
+++ perceptron.py
@@ -51,7 +51,7 @@ df.drop(['id'],1, inplace=True)
 
 ### this part could be done using sklearn ###
 
-#set all to values as floats
+#set all values as floats
 full_data = df.astype(float).values.tolist()
 
 #shuffle the data
