2bf7397ca3cfae12c4376ccd70f68d239e20387c
anthony <anthonyjbet@gmail.com>
2019-02-18 01:37:13 -0300

    i replaced the NAN data with the average of the column, this change made the alg more acurate

--- perceptron.py
+++ perceptron.py
@@ -3,6 +3,7 @@ import random
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
 class perceptron(object):
 
     def training(self,data,dim):
@@ -13,7 +14,6 @@ class perceptron(object):
         self.b = 0
         self.k = None
         self.R = np.amax([np.linalg.norm(element[:-1]) for element in self.data]) #R = max{norm({all_points}})
-        print(self.R)
         step = 0
 
         while self.k != 0:
@@ -27,12 +27,12 @@ class perceptron(object):
                     self.w = self.w + (self.n * self.data[i][-1] * self.data[i][:-1])
                     self.b = self.b + (self.n * self.data[i][-1]*(self.R**2))
                     self.k += 1
-
+        """
         print(self.b)
         print(self.w)
         print(self.k)
         print('done')
-                
+         """       
     def predict(self,new_point):
 
         pred = (np.dot(self.w,new_point) + self.b)
@@ -46,8 +46,15 @@ class perceptron(object):
 
 
 df = pd.read_csv('breast-cancer-wisconsin.data')
-df.replace('?',-99999, inplace=True)
 df.drop(['id'],1, inplace=True)
+
+#did it 'couse the Bare_Nuclei colum was object type, not numeric, so pandas wasnt showing the 
+#the mean() of this column 
+#df['Bare_Nuclei'] = df.Bare_Nuclei.convert_objects(convert_numeric=True) or
+df['Bare_Nuclei'] = pd.to_numeric(df.Bare_Nuclei,downcast='integer', errors='coerce')
+
+df.fillna(df.mean(),inplace=True)
+
 
 ### this part could be done using sklearn ###
 
@@ -73,48 +80,32 @@ test_data = np.array(full_data[-int(test_size*len(full_data)):])
 
 ##### test the alg in some real data####
 
-total = 0
-correct = 0
-bound = 0
+soma = 0
+for time in range(0,10000):
 
-a = perceptron()
-a.training(train_data,9)
+    total = 0
+    correct = 0
+    bound = 0
 
-for test in test_data:
-    
-    if a.predict(test[:-1]) == test[-1]:
-        correct += 1 
-    elif a.predict(test[:-1]) == 0:
-        bound += 1
-    
-    total += 1
+    a = perceptron()
+    a.training(train_data,9)
 
-print(correct,bound,total,correct/total)
+    for test in test_data:
+        
+        if a.predict(test[:-1]) == test[-1]:
+            correct += 1 
+        elif a.predict(test[:-1]) == 0:
+            bound += 1
+        
+        total += 1
 
+    #print(correct,bound,total,correct/total)
+    soma += correct/total
+
+print(soma/10000)
 ################
 
-### linear sep dataset ###
-"""
-separ_data = np.array([[-1,-1,-1],[-2,-4,-1],[2,2,1],[3,5,1],[7,9,1]])
 
-total = 0
-correct = 0
-bound = 0
-
-a = perceptron()
-a.training(separ_data)
-
-for test in separ_data:
-    if a.predict(test[:-1]) == test[-1]:
-        correct += 1 
-    elif a.predict(test[:-1]) == 0:
-        bound += 1
-    
-    total += 1
-
-print(correct,bound,total,correct/total)
-"""
-##########
 
 def plot_data(data):
     for point in data:
